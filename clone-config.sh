7fa175ea145303f32128d320cd25aad3aff00d2e
rutvik2611 <rutvik2611@gmail.com>
2023-03-06 22:20:39 -0500

    modified:   clone-config.sh

--- clone-config.sh
+++ clone-config.sh
@@ -11,11 +11,11 @@ git config --global user.name $username
 git config --global user.password $password
 
 # Check if login is successful
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://api.github.com/user)
-if [ "$response" -ne 200 ]; then
-  echo "Failed to authenticate to GitHub. Please check your credentials."
-  exit 1
-fi
+# response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://api.github.com/user)
+# if [ "$response" -ne 200 ]; then
+#   echo "Failed to authenticate to GitHub. Please check your credentials."
+#   exit 1
+# fi
 
 cd $HOME && git clone https://rutvik2611:${password}@github.com/rutvik2611/data_config.git
 echo "Git login successful"
