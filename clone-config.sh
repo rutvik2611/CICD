eeeaa528335dab1f3898c945b82ea5eca2a3d46c
rutvik2611 <rutvik2611@gmail.com>
2023-03-06 21:54:55 -0500

    update clone-config.sh

--- clone-config.sh
+++ clone-config.sh
@@ -3,7 +3,9 @@
 # Prompt for username and password
 read -p "Enter your Git username: " username
 read -s -p "Enter your Git password or personal access token: " password
-echo ""
+echo "${username}"
+echo "${password}"
+
 
 # Clone the repository using the provided credentials
 git clone https://${username}:${password}@github.com/rutvik2611/data_config.git
