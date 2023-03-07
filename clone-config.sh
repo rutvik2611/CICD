b2bad02fe4750a66537ee312f0320831fb9449dd
rutvik2611 <rutvik2611@gmail.com>
2023-03-06 22:15:28 -0500

    update clone-config.sh

--- clone-config.sh
+++ clone-config.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
-
 # Prompt for username and password
 read -p "Enter your Git username: " username
-read -s -p "Enter your Git password or personal access token: " password # If you're using token, paste it here.
+read -s -p "Enter your Git password or personal access token: " password
 
 echo ""
 
@@ -13,9 +11,12 @@ git config --global user.name $username
 git config --global user.password $password
 
 # Check if login is successful
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://api.github.com/user)
+if [ "$response" -ne 200 ]; then
+  echo "Failed to authenticate to GitHub. Please check your credentials."
+  exit 1
+fi
 
 # Clone the repository using the provided credentials
 git clone https://${username}:${password}@github.com/rutvik2611/data_config.git
 echo "Git login successful"
-
-
